7c420e08955d1a52b69ed059632195e0372855ea
Test <test@example.com>
2024-10-17 19:05:52 +0200

    SpringBoot

---
+++ src/test/java/semester3/webtech/WebtechApplicationTests.java
@@ -0,0 +1,13 @@
+package semester3.webtech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebtechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
