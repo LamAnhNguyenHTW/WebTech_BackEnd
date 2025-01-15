e979a9f47ce170130ad2bc8a53cc8291040f9a73
Test <test@example.com>
2025-01-15 13:02:42 +0100

    BackendTest

--- src/main/java/semester3/webtech/config/WebConfig.java
+++ src/main/java/semester3/webtech/config/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig implements WebMvcConfigurer {
                 .addMapping("/**")
                 .allowedMethods("*")
                 .allowedOrigins(
-                        "http://localhost:5184",
+                        "http://localhost:5173",
                         "https://webtech-frontend-qfu2.onrender.com"
                 );
     }
